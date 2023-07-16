72fc8542941740d1b80a8f5cc6ba300097dd3070
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2023-07-16 11:34:45 +0530

    Add files via upload

---
+++ JAVAProject01/Book.java
@@ -0,0 +1,27 @@
+public class Book extends LibraryItem {
+
+	private String author;
+	private int numPages;
+	
+	Book(String title,
+		String itemId,
+		String author,
+		int numPages) {
+			
+		super(title, itemId);
+		this.author = author;
+		this.numPages = numPages;
+	}
+	
+	public void displayItemDetails(){
+		System.out.println("Item ID : " + getItemId() + "\n" + "Title : " + getTitle() + "\n" + "Author : " + author + "\n" + "Number of pages : " + numPages);
+	}
+	
+	public String checkOut() {
+		return author;
+	}
+	
+	public String checkIn() {
+		return author;
+	}
+}
